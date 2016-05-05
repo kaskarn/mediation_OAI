c7ae9386955226877bb1ce3e776b7b0730d57a97
kaskarn <baldassarian@gmail.com>
2016-05-05 14:27:48 -0400

    starting g formula stuff

--- main_new.R
+++ main_new.R
@@ -1,7 +1,7 @@
 library(magrittr); library(mice)
 library(foreign); library(dplyr)
 library(reshape2); library(ipw)
-library(nnet); library(foreach)
+library(foreach)
 
 whichtimes <- function (stub, data = df, plen = 3){
   grep(paste0(stub, "$"), colnames(data), value = TRUE) %>%
@@ -15,14 +15,10 @@ CreateTableOne(data = df, vars = c("v00lfmaxf", "v00pase", "v00abcirc"), strata 
 #knee function : womac disability // KOOS function
 #physical exam : 20 m // isometric strength
 #chronic disease : hypertension // diabetes
-#
-
-## to do: model specification, use IPW package
-
 
 ##### Read data from .csv downloaded at epi-ucsf.edu #####
 tyears <- c(0,1,1.5,2,2.5,3:8)
-times <- c(0,3,6,10)
+times <- c(0:10)
 df <- left_join(read.csv("Enrollees.txt", sep = "|"), read.csv("Outcomes99.txt", sep = "|") %>% rename(ID = id), by = "ID")
 for(i in times) df <- left_join(df, read.csv(paste0("AllClinical", sprintf("%02d",i), ".txt"), sep = "|"), by = "ID")
 colnames(df) <- tolower(colnames(df)) #fix SAS case-agnostic names
@@ -43,15 +39,15 @@ for(i in 1:length(times[-1])) d2[is.na(d2[ageloc+i]),ageloc+i] <- d2[is.na(d2[ag
 
 ##### Meta stuff ##### 
 N <- nrow(df)
-impnum <- 7; bnum <- 100
+impnum <- 5; bnum <- 50
 outcomes <- c("20mpace", "400mtim", "hspss")
 mediators <- c("obcat", "pycat", "depcat")
-confounders <- c("age", "p02sex", "p02race")
-exposure <- "v00income"
+confounders <- c("age", "p02sex", "p02race", "agesq", "a_ra", "a2_ra", "a_se", "a2_se", "ra_se")
+exposure <- "v00edcv"
 len <- length(unique(d3[[exposure]]))
 
 ##### Multiple imputation using MICE #####
-d2_imp <- mice(d2, m = impnum, pred=quickpred(d2, minpuc = .25, include = c("v00age", "p02sex", "p02race"), exclude = c(meta))) %>%
+d2_imp <- mice(d2, m = impnum, pred=quickpred(d2,  include = c("v00age", "p02sex", "p02race"), exclude = meta)) %>%
   complete(., action = "long", include = FALSE)
 
 ##### convert to wide format #####
@@ -61,10 +57,17 @@ dl <- lapply(long, function (i) melt(d2_imp[match(c(".imp", meta, xsec, i), coln
                                 mutate(time = substr(i,2,3)[as.numeric(time)]))
 d3 <- Reduce(function (...) merge(..., all = TRUE, by=c(meta, xsec, "time", ".imp")), dl)
 
-##### Categorize mediators ##### 
-d3 %<>% mutate(obcat = cut(bmi, breaks = c(min(bmi, na.rm = T),25,30,35, max(bmi, na.rm = T))),
-              depcat = cut(hsmss, breaks = c(0, 40, 50, 60, 100)),
-              pycat = .bincode(smkpkyr, breaks = c(0,10,20,30, max(d3$smkpkyr, na.rm = T)), right = TRUE))
+##### Categorize mediators and create interaction terms##### 
+d3 %<>% mutate(obcat = cut(bmi, breaks = c(min(bmi, na.rm = T),25,30,35, 999)),
+              depcat = cut(hsmss, breaks = c(0, 40, 50, 60, 999)),
+              pycat = .bincode(smkpkyr, breaks = c(0,10,20,30, 999)),
+              agesq = age*age,
+              ra_se = (p02race %in% "AA")*(p02sex == 2),
+              a_ra = (p02race %in% "AA")*age,
+              a2_ra = (p02race %in% "AA")*agesq,
+              a_se = (p02sex == 2)*age,
+              a2_se = (p02sex == 2)*agesq
+              )
 d3[d3$smkpkyr %in% 0,"pycat"] <- 0
 d3$pycat <- factor(d3$pycat, labels = c("0", "0-10", "10-20", "20-30", "30+"))
 
@@ -86,16 +89,19 @@ res_b <- res_b_g <- array(NA,
                     dimnames = list(1:impnum,1:bnum, outcomes, mediators))
 
 ###### Analyses education #####
+
 exposure <- "v00edcv"
 pb <- txtProgressBar(style=3); fin <- 0
+d4 <- d3[d3$.imp %in% 1:3,]
 for(i in levels(d3$.imp)){
   #timp <- d3[d3$.imp == i,]
   for(j in 1:bnum){
     tsamp <- d3[d3$.imp == i,][d3[d3$.imp == i,]$id %in% sample(df$id, N, replace = TRUE),]
     wa_num <-  multinom(get(exposure) ~ 1, data = tsamp, trace = FALSE) %$% 
       predict(object = ., newdata = tsamp, "probs")
-    wa_den <- multinom(get(exposure) ~ p02sex + p02race + age + age*age, data = tsamp, trace = FALSE) %$% 
-      predict(object = ., newdata = tsamp, "probs")
+    wa_den <- multinom(get(exposure) ~ p02sex + p02race + age + age*age + p02sex*p02race + 
+                         age*p02sex + age*p02race, data = tsamp, trace = FALSE) %$% 
+              predict(object = ., newdata = tsamp, "probs")
     tsamp$wa <- (wa_num / wa_den)[cbind(1:nrow(tsamp),as.numeric(tsamp[[exposure]]))]
     
     wm_num.models <- lapply(mediators, function (i) multinom(data = tsamp, get(i) ~ get(exposure), trace = FALSE))
@@ -106,6 +112,9 @@ for(i in levels(d3$.imp)){
       tsamp[[paste0("wm_", mediators[m])]] <- wm_tmp[cbind(1:nrow(tsamp),as.numeric(tsamp[[mediators[m]]]))]
     }
     for(o in 1:length(outcomes)){
+      for(k in len){
+        
+      }
       res_a[i,j,o,1,] <- lm(data = tsamp, get(outcomes[o]) ~ get(exposure))$coefficients[2:(len)]
       res_a[i,j,o,2,] <- lm(data = tsamp, get(outcomes[o]) ~ get(exposure), weights = wa)$coefficients[2:len]
       for(m in 1:length(mediators)){
@@ -123,14 +132,7 @@ for(i in 1:length(mediators)){
   print(mediators[i])
   for(o in 1:length(outcomes)){
     print(outcomes[o])
-    print(apply((res_inc.a[,,o,2,] - res_inc.b[,,o,i,]), 3, quantile, probs = c(0.025, 0.5, 0.975))) #/res_inc.a[,,o,2,]
-  }
-}
-for(i in 1:length(mediators)){
-  print(mediators[i])
-  for(o in 1:length(outcomes)){
-    print(outcomes[o])
-    print(apply((res_a[,,o,2,] - res_b[,,o,i,])/res_a[,,o,2,], 3, quantile, probs = c(0.025, 0.5, 0.975))) #
+    print(apply((res_a[,,o,2,] - res_b[,,o,i,])/res_a[,,o,i,], 3, quantile, probs = c(0.025, 0.5, 0.975))) #
   }
 }
 
