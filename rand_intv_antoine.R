dc4bbfbcffaf3e8baa56a96f1f5d5720b7f26075
kaskarn <baldassarian@gmail.com>
2016-06-19 23:12:50 -0400

    Allows comparing

--- rand_intv_antoine.R
+++ rand_intv_antoine.R
@@ -43,23 +43,37 @@ rint_med.mkdata <- function(orig_dat, X, M, Y, C = "", L){
   rint_med.mknum <- function(dat, M, L, X, mmod, lmod){
     ## get p( l | a*,c) for each value in support of L
     plasc <- predict(object = lmod, newdata = mutate_(dat, .dots = setNames(list(~astar), X)), type = "probs")
+    if(is.null(dim(plasc))) plasc <- cbind(1-plasc, plasc)
     
     ## get p( m | l,a*,c) for each value in support of L
     pmlasc <- lapply(levels(dat[[L]]), function (i) 
                       predict(object = mmod, newdata = mutate_(dat, .dots = setNames(list(~astar), X)) %>%
-                                                       mutate_(.dots = setNames(list(~i), M)),
+                                                       mutate_(.dots = setNames(list(~i), L)),
                               type = "probs") %>% fmatch(dat[[M]])
                      ) %>% do.call(cbind, .)
     
     ## sum p( m | l,a*,c ) * p( l | a*,c ) across levels of L
     num <- apply((plasc * pmlasc), 1, sum) 
-    return(list(plasc = plasc, pmlasc = pmlasc, num = num))
+    
+    
+    #Same pmlasc as AK
+    pmlasc_ak <- predict(object = mmod, 
+                       newdata = mutate_(dat, .dots = setNames(list(~astar), X)),
+                       type = "probs") %>% fmatch(dat[[M]])
+    num_ak <- apply((plasc * pmlasc_ak), 1, sum)
+    return(list(plasc = plasc, pmlasc = pmlasc, num = num, num_ak = num_ak))
   }
   
   rint_med.mkweight <- function(dat){
     w <- dat$num/dat$den
     return(w)
   }
+  #To get same weights as AK
+  rint_med.mkweight_ak <- function(dat){
+    w <- dat$num_ak/dat$den
+    return(w)
+  }
+  
   ## create models
   flist <- rint_med.mkform(X, C, L, M)
   mlist <- rint_med.mkmods(orig_dat, flist)
@@ -76,9 +90,15 @@ rint_med.mkdata <- function(orig_dat, X, M, Y, C = "", L){
   num_items <- rint_med.mknum(an_dat, M, L, X, mmod = mlist$m_mod, lmod = mlist$l_mod)
   an_dat$num <- num_items$num
 
-  ## calculate weight
+  ## calculate weight (regular)
   an_dat$w <- rint_med.mkweight(an_dat)
   
+  
+  
+  # get same weights as AK
+  an_dat$num_ak <- num_items$num_ak
+  an_dat$w_ak <- rint_med.mkweight_ak(an_dat)
+    
   return(list(
     models = mlist,
     an_dat = an_dat,
@@ -88,7 +108,11 @@ rint_med.mkdata <- function(orig_dat, X, M, Y, C = "", L){
     pmlasc = num_items$pmlasc,
     den = an_dat$den,
     num = an_dat$num,
-    w = an_dat$w
+    w = an_dat$w,
+    
+    #same weights as AK
+    w_ak = an_dat$w_ak,
+    num_ak = num_items$num_ak
   ))
 }
 
@@ -98,15 +122,24 @@ rint_med.decompose <- function(dat, Y, X, astar = "astar"){
   m_ter <- lm(data = dat[dat[[astar]] == dat[[X]],], as.formula(paste0(Y, "~", astar)), weights = w)
   m_nder <- lm(data = dat[dat[[astar]] == ref,], as.formula(paste0(Y, "~", X)), weights = w)
   m_nier <- lm(data = dat[dat[[X]] != ref,], as.formula(paste0(Y, "~", astar)), weights = w)
+  nier <- lapply(levels(dat[[X]])[-1], function(i) lm(data = dat[dat[[X]] == i,], 
+                                                     as.formula(paste0(Y, "~", astar)), weights = w)$coefficients[-1]
+                ) %>% unlist
   
   return(list(
     models = list(m_te = m_te, m_ter = m_ter, m_nder = m_nder, m_nier = m_nier),
     nder = m_nder$coefficients[-1],
-    nier = m_nier$coefficients[-1],
+    nier = nier,
+    nier2 = m_nier$coefficients[-1],
     ter = m_ter$coefficients[-1],
     te = m_te$coefficients[-1]
   ))
 }
 
+rint_med2 <- function(dat, Y, X, astar = "astar"){
+  ref <- levels(dat[[astar]])[1]
+  
+  return(unlist(nie))
+}
 
   
